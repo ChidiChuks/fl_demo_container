0f031b72a802fd238b1e9371996f7265b62b2e4e
ChidiChuks <chukschiboy@gmail.com>
2020-05-04 12:05:18 +0100

    update on tiger image with rounded container shape

--- lib/main.dart
+++ lib/main.dart
@@ -14,22 +14,29 @@ class MyApp extends StatelessWidget {
         body: Column(
           children: <Widget>[
             Container(
-              margin: EdgeInsets.all(100.0),
+              margin: EdgeInsets.all(50.0),
               width: 300.0,
               height: 300.0,
+              // child: FlutterLogo(),
               decoration: BoxDecoration(
                 color: Colors.orange,
-                gradient: RadialGradient(
-                  radius: 0.25,
-                  center: Alignment(0.3, 0.5),
-                  tileMode: TileMode.mirror,
-                  colors: [Colors.purple[50], Colors.purple[500]]
+                // gradient: LinearGradient(
+                //   colors: [Colors.brown[50], Colors.brown[300]],
+                // ),
+                image: DecorationImage(
+                  image: new AssetImage('assets/tiger.png'),
                 ),
-                shape: BoxShape.rectangle,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(25.0),
-                  bottomRight: Radius.circular(25.0),
-                ),
+                // gradient: RadialGradient(
+                //   radius: 0.25,
+                //   center: Alignment(0.3, 0.5),
+                //   tileMode: TileMode.mirror,
+                //   colors: [Colors.purple[50], Colors.purple[500]]
+                // ),
+                shape: BoxShape.circle,
+                // borderRadius: BorderRadius.only(
+                //   topLeft: Radius.circular(25.0),
+                //   bottomRight: Radius.circular(25.0),
+                // ),
               ),
             ),
           ],
